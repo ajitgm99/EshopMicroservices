ab01bcd4c68b2fdc880ccc06e19b3a1e1dc72476
Ajit Mhatre <ajitgm99@gmail.com>
2024-08-24 00:36:05 +0530

    Create Handler and Save in the Postgres SQl

--- src/Services/Catalog/catalog.API/Program.cs
+++ src/Services/Catalog/catalog.API/Program.cs
@@ -2,8 +2,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the container
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(option =>
+{
+    option.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+
+app.MapCarter();
 
 app.Run();
