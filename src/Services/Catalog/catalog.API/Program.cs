c8c52b9bc2d25979c7a55c872df165eb6d0d3f4e
Ajit Mhatre <ajitgm99@gmail.com>
2024-09-02 02:20:38 +0530

    Global Exception and Logging Behaviour

--- src/Services/Catalog/catalog.API/Program.cs
+++ src/Services/Catalog/catalog.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 
 });
 
@@ -22,10 +24,15 @@ builder.Services.AddMarten(option =>
     option.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
